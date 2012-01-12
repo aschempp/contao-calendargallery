699c762e75d6e3bb9cc24dc5a3b031c2e8adc21e
Dominik Zogg <dominik.zogg@gmail.com>
2012-01-12 23:24:08 +0100

    update copyright from typolight to contao update date add me as an author

--- languages/en/tl_calendar_events.php
+++ languages/en/tl_calendar_events.php
@@ -1,28 +1,30 @@
-<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
 
 /**
- * TYPOlight webCMS
- * Copyright (C) 2005 Leo Feyer
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
- * version 2.1 of the License, or (at your option) any later version.
- * 
+ * version 3 of the License, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
- * Software Foundation website at http://www.gnu.org/licenses/.
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
  *
  * PHP version 5
- * @copyright  Andreas Schempp 2009
+ * @copyright  Andreas Schempp 2011
  * @author     Andreas Schempp <andreas@schempp.ch>
+ * @author     Dominik Zogg <dominik.zogg@gmail.com>
  * @license    http://opensource.org/licenses/lgpl-3.0.html
- * @version    $Id$
  */
 
 
@@ -53,4 +55,3 @@ $GLOBALS['TL_LANG']['tl_calendar_events']['meta']      			= 'Meta file (meta.txt
  * Legends
  */
 $GLOBALS['TL_LANG']['tl_calendar_events']['gallery_legend']		= 'Event gallery';
-
